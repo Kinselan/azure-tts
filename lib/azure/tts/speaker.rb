08500f41e51f3690f0103f4ad52b2421932859e7
Daniel Nalesnik <nalesnik-m1@Daniels-Mac-mini.local>
2021-07-17 13:13:21 +0800

    typo

--- lib/azure/tts/speaker.rb
+++ lib/azure/tts/speaker.rb
@@ -19,7 +19,7 @@ module Azure
       def ssml
         <<~HEREDOC
           <speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US">
-            <voice name=#{@voice_short_name}>
+            <voice name="#{@voice_short_name}">
               <prosody rate="#{@rate}" pitch="0%">
                 #{@text}
               </prosody>
